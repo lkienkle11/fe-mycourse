4d7c638c08d8ad3ceed3ce48dc359d003c7d5499
kienlt <kienlt@gmail.com>
2026-03-31 15:33:04 +0700

    feat: add custom fonts and assets

---
+++ public/assets/icons/types.ts
@@ -0,0 +1,8 @@
+export interface ISvgProps {
+    className?: string;
+    color?: string;
+    width?: number;
+    height?: number;
+    outlineColor?: string;
+    // id: string; const id = useId().replace(/:/g, '');
+}
