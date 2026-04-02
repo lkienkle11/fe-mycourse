7c8524f7fc0a2729ba71e78e079de0943bda2e05
kienlt <kienlt@gmail.com>
2026-04-02 16:04:21 +0700

    feat: add auth form ui primitives

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import * as React from "react";
+import type * as React from "react";
 
 import { cn } from "@/lib/utils";
 
 function Label({ className, ...props }: React.ComponentProps<"label">) {
   return (
+    // biome-ignore lint/a11y/noLabelWithoutControl: <explanation>
     <label
       data-slot="label"
       className={cn(
