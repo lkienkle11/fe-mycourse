f1d2ea919f22c48304732606e7ee360755206dd4
kienlt <kienlt@gmail.com>
2026-04-04 12:56:09 +0700

    feat: add auth hooks

---
+++ src/hooks/context/auth/useAuthContext.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useContext } from "react";
+import { AuthContext, type AuthContextValue } from "@/context/auth";
+
+export function useAuthContext(): AuthContextValue {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuthContext must be used within an <AuthProvider>.");
+  }
+  return context;
+}
